075d8dbfe71cd07d7f18d03c043a253b63cd7c35
domymontalto <mon21002@byui.edu>
2022-06-17 11:53:27 -0600

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Domenico Montalto on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
